ef2f5f4ddd0e95a70b57a2175f8fbf129ddf9649
AndresLince <lynxdarmatron@gmail.com>
2023-07-22 21:32:10 -0500

    Add constant

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -37,22 +37,7 @@ class _CityScreenState extends State<CityScreen> {
                   style: TextStyle(
                     color: Colors.black
                   ),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    icon: Icon(
-                      Icons.location_city,
-                      color: Colors.white,
-                    ),
-                    hintText: 'Enter city name',
-                    hintStyle: TextStyle(
-                      color: Colors.grey
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide.none
-                    )
-                  ),
+                  decoration: ktextFieldInputDecoration
                 ),
               ),
               TextButton(
